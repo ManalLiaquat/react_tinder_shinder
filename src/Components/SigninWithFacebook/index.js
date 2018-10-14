d816f71b2c2503894c039953fa0317b2291cb815
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-15 02:36:46 +0500

    redirecting

--- src/Components/SigninWithFacebook/index.js
+++ src/Components/SigninWithFacebook/index.js
@@ -3,12 +3,12 @@ import { Button } from "@material-ui/core";
 import firebase from "../../Config/firebase";
 import Toast from "../../Constants/Toast";
 
-const login = () => {
+const login = (props) => {
   var provider = new firebase.auth.FacebookAuthProvider();
   firebase
     .auth()
     .signInWithPopup(provider)
-    .then(function(result) {
+    .then(function (result) {
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       var token = result.credential.accessToken;
       // console.log(token);
@@ -16,14 +16,15 @@ const login = () => {
         type: "success",
         title: "Signed in successfully",
         onClose: () => {
-          window.location.reload();
+          window.location.pathname = '/';
         }
       });
+
       // The signed-in user info.
       var user = result.user;
       // console.log(user);
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -39,7 +40,7 @@ const login = () => {
 
 const SigninWithFacebook = () => {
   return (
-    <Button variant="contained" color="primary" onClick={login}>
+    <Button variant="outlined" color="inherit" onClick={login}>
       Signin with Facebook
     </Button>
   );
