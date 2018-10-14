4456f17868f037d5335dae6a018f8c420b8690c4
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-15 02:31:26 +0500

    Rendering NavBar on all pages by customRoutes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./Components/App";
+import CustomRoutes from "./Routes";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<CustomRoutes />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
