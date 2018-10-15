6d7938c4fcb3af0a8889642d7ea9a7e016c2dde2
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-15 23:51:24 +0500

    creating a profile route

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,9 +1,10 @@
 
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import AppBar from "../Components/MaterialUI/AppBar";
 import * as CheckUser from "../Constants/CheckUser";
+import AppBar from "../Components/MaterialUI/AppBar";
 import Home from "../Components/Home";
+import Profile from "../Components/Profile";
 
 
 const CustomRoutes = () => (
@@ -11,7 +12,7 @@ const CustomRoutes = () => (
     <div>
       <Route path="/" render={(props) => <AppBar {...props} user={CheckUser.User} />} />
       <Route path="/home" render={(props) => <Home {...props} user={CheckUser.User} />} />
-      {/* <Route path="/profile" component={bbb} /> */}
+      <Route path="/profile" render={(props) => <Profile {...props} user={CheckUser.User} />} />
     </div>
   </Router>
 );
