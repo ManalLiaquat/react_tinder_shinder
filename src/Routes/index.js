bc61113a6d6702a24e78471447d1be9954b5dd7e
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-15 20:49:56 +0500

    creating home route

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,17 +3,15 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AppBar from "../Components/MaterialUI/AppBar";
 import * as CheckUser from "../Constants/CheckUser";
-import App from "../Components/App";
+import Home from "../Components/Home";
+
 
 const CustomRoutes = () => (
   <Router>
     <div>
-      <AppBar user={CheckUser.User} />
-
-      <Route exact path="/" component={App} />
-      {/* <Route path="/aaa" component={aaa} /> */}
-      {/* <Route path="/bbb" component={bbb} /> */}
-      {/* <Route path="/ccc" component={ccc} /> */}
+      <Route path="/" render={(props) => <AppBar {...props} user={CheckUser.User} />} />
+      <Route path="/home" render={(props) => <Home {...props} user={CheckUser.User} />} />
+      {/* <Route path="/profile" component={bbb} /> */}
     </div>
   </Router>
 );
