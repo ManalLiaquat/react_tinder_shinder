b48e07833986b739fdfae86f41195a06b212d2bc
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-16 22:07:47 +0500

    using material-ui stepper component

---
+++ src/Components/MaterialUI/Stepper/index.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Stepper, Step, StepLabel, StepContent, Button, Paper, Typography } from "@material-ui/core";
+import Submit from "./Submit";
+import Step1 from './Submit/GetUserData/Step1'
+
+const styles = theme => ({
+  root: {
+    width: '90%',
+  },
+  button: {
+    marginTop: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing.unit * 2,
+  },
+  resetContainer: {
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+var getNameObj;
+function getName(nickName, phone) {
+  getNameObj = { nickName, phone };
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <Step1 getNameAndPhone={getName} />;
+    case 1:
+      return 'An ad group contains one or more ads which target a shared set of keywords.';
+    case 2:
+      return `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`;
+    case 3:
+      return 'Render Maps';
+    default:
+      return 'Unknown step';
+  }
+}
+
+function getSteps() {
+  return ['Enter your nick name and phone number', 'Upload 3 images (necessary)', 'Select Beverages', 'Set your location'];
+}
+
+class VerticalLinearStepper extends React.Component {
+  state = {
+    activeStep: 0
+  };
+
+  handleNext = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep + 1,
+    }));
+  };
+
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1,
+    }));
+  };
+
+  // handleReset = () => {
+  //   this.setState({
+  //     activeStep: 0,
+  //   });
+  // };
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Stepper activeStep={activeStep} orientation="vertical">
+          {steps.map((label, index) => {
+            return (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+                <StepContent>
+                  <Typography>{getStepContent(index)}</Typography>
+                  <div className={classes.actionsContainer}>
+                    <div>
+                      <Button
+                        disabled={activeStep === 0}
+                        onClick={this.handleBack}
+                        className={classes.button}
+                      >
+                        Back
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleNext}
+                        className={classes.button}
+                      >
+                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                      </Button>
+                    </div>
+                  </div>
+                </StepContent>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length && (
+          <Paper square elevation={0} className={classes.resetContainer}>
+            <Typography>All steps completed - you&quot;re finished</Typography>
+            {/* RENDER Submit COMPONENT */}
+            <Submit step1={getNameObj} />
+          </Paper>
+        )}
+      </div>
+    );
+  }
+}
+
+VerticalLinearStepper.propTypes = {
+  classes: PropTypes.object,
+};
+
+export default withStyles(styles)(VerticalLinearStepper);
