8ab6afb0a7bce472d909551ae0a7721ad63d5e8b
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-19 00:22:11 +0500

    fixing bug

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import { Typography } from "@material-ui/core";
 import Stepper from "../MaterialUI/Stepper";
 
-const Profile = () => {
+const Profile = (props) => {
   return (
     <div>
       <Typography variant="h5" align="center" >Fill all fields, upload all (3) images, select atleast one from two diffrent questions and turn on location (accept location access).</Typography>
-      <Stepper {...this.props} />
+      <Stepper {...props} />
     </div>
   );
 }
