9656a08bdc9eba12a912ccedd2a85a677196d27a
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-18 23:43:21 +0500

    This component will render all users matched by the current user's meeting requirement data

---
+++ src/Components/Meetings/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Typography, Button } from "@material-ui/core";
+import firebase from "../../Config/firebase";
+import Wrapper from "../UserCard";
+
+class Meetings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+  }
+
+  getAllUsers() {
+    firebase.database().ref('/user_data').on("child_added", data => {
+      let users = data.val();
+      console.log(users);
+
+    })
+  }
+
+  componentDidMount() {
+    this.getAllUsers()
+  }
+
+  render() {
+    // const { user } = this.props;
+    // console.group("MEETINGS")
+    // console.log(this.props, "****props");
+    // console.groupEnd()
+
+    return (
+      <div >
+        <div style={{ textAlign: "center" }}>
+          <Typography variant="h6" style={{ lineHeight: "100px" }}>Show all users with card-swipe-deck | card-swing</Typography>
+          <Wrapper></Wrapper>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Meetings;
