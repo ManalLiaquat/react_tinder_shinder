f324735f992b42034af5d1438b06a40c7949ff26
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-18 23:59:20 +0500

    No use of state so change this component to stateless component

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,29 +1,14 @@
 import React, { Component } from "react";
-// import firebase from "../../Config/firebase";
-// import GoogleMap from "../GoogleMap";
-// import * as CheckUser from "../../Constants/CheckUser";
 import { Typography } from "@material-ui/core";
 import Stepper from "../MaterialUI/Stepper";
 
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    // const { user } = this.props;
-    console.group("PROFILE")
-    console.log("{props}\n", this.props);
-    console.groupEnd()
-    return (
-      <div >
-        <Typography variant="h5" align="center" >Fill all fields, upload all (3) images, select atleast one from two diffrent questions and turn on location (accept location access).</Typography>
-        <Stepper {...this.props} />
-      </div>
-    );
-  }
+const Profile = () => {
+  return (
+    <div>
+      <Typography variant="h5" align="center" >Fill all fields, upload all (3) images, select atleast one from two diffrent questions and turn on location (accept location access).</Typography>
+      <Stepper {...this.props} />
+    </div>
+  );
 }
 
-export default Dashboard;
+export default Profile;
