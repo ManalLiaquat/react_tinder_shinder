c4f141c84d6e5dfb27e8120c1ee3b95c933d12b8
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-20 23:08:11 +0500

    Using this component as a main component which renders navbar on all routes

---
+++ src/Components/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AppBar from '../MaterialUI/AppBar';
+
+const App = (props) => {
+  return (
+    <span>
+      <AppBar {...props} />
+    </span>
+  )
+}
+
+export default App;
