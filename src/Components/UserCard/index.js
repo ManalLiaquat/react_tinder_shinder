d38a79530e63b770fff892e8f29cad48255c7327
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-21 23:49:49 +0500

    this will matching all the users with currentUser's profile

--- src/Components/UserCard/index.js
+++ src/Components/UserCard/index.js
@@ -1,45 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import Cards, { Card } from "react-swipe-deck";
-import { Card as MUICard } from "@material-ui/core"
+import MUICard from "../MaterialUI/Card";
+import firebase from "../../Config/firebase";
+import * as CheckUser from "../../Constants/CheckUser";
+import geofire from "geofire";
+
 import img1 from "../../Images/img1.jpg";
 import img2 from "../../Images/img2.jpg";
 import img3 from "../../Images/img3.jpg";
 
 
-let action = msg => {
-  console.log(msg);
-};
-
-const data = ["Alexandre", "Thomas", "Lucien"];
 const images = [img1, img2, img3];
 
-const Wrapper = () => {
-  return (
+const data = ["user one obj", " user two obj", "user three obj"];
 
-    <Cards onEnd={action("end")} className="master-root">
-      {data.map(item => (
-        <Card
-          onSwipeLeft={() => { action("swipe left") }}
-          onSwipeRight={() => { action("swipe right") }}
-        >
-          <MUICard>
-            <Cards onEnd={action("end")} className="master-root">
-              {
-                images.map(path => (
-                  <Card
-                    onSwipeLeft={() => { action("swipe left") }}
-                    onSwipeRight={() => { action("swipe right") }}>
-                    <img src={path} width="100%" height="100px" alt="user" />
-                  </Card>
-                ))
-              }
-            </Cards>
-            <h2>{item}</h2>
-          </MUICard>
-        </Card>
-      ))}
-    </Cards>
-  );
+class UserCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allUsers: [],
+      myLocation: props.myLocation,
+      myOptions: props.myOptions
+    }
+  }
+
+  getAllUsers() {
+    let { allUsers, myLocation, myOptions } = this.state
+
+    firebase.database().ref('/user_data').on("child_added", data => {
+      let user = data.val();
+
+      if (user.uid !== CheckUser.User.uid) {
+        let userLocation = [user.location.latitude, user.location.longitude];
+        let distance = geofire.distance(myLocation, userLocation).toFixed(3);
+
+        let compareBeverages = myOptions.beverages.some(value => user.beverages.includes(value))
+        let compareTime = myOptions.time.some(value => user.time.includes(value))
+
+        if (distance <= 5 && compareBeverages && compareTime) {
+          allUsers.push(user)
+          this.setState({ allUsers })
+        }
+      }
+    })
+
+  }
+
+  componentDidMount() {
+    this.getAllUsers()
+  }
+
+  action = msg => {
+    // console.log(msg);
+  };
+
+  render() {
+    let { allUsers } = this.state
+    // console.log(allUsers);
+
+    return (
+      <Cards onEnd={this.action("end")} className="root-master">
+        {allUsers.map(item => (
+          <Card
+            onSwipeLeft={() => { this.action("swipe left") }}
+            onSwipeRight={() => { this.action("swipe right") }}
+          >
+            <MUICard images={item.images} nickName={item.nickName}>
+              <p>{item.displayName}</p>
+            </MUICard>
+          </Card>
+        ))}
+      </Cards>
+    );
+  }
 };
 
-export default Wrapper;
+export default UserCard;
