1ffe84ed3a0e95bf5658c6235d10005ce03635f5
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-24 23:22:15 +0500

    calling firebase at startup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import CustomRoutes from "./Routes";
 import * as serviceWorker from "./serviceWorker";
+import "./Config/firebase";
 
 ReactDOM.render(<CustomRoutes />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 if (module.hot) {
   module.hot.accept()
