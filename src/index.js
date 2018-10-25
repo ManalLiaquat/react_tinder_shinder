0f3db9d7b6644e1e45a2e14d293b28c771d7d9c3
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-25 23:50:13 +0500

    getting props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import CustomRoutes from "./Routes";
 import * as serviceWorker from "./serviceWorker";
 import "./Config/firebase";
 
-ReactDOM.render(<CustomRoutes />, document.getElementById("root"));
+ReactDOM.render(<Router><CustomRoutes /></Router>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
