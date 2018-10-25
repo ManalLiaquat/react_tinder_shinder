14fe3087b2ff6f1de18b18774dd3a22ce411ae73
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-25 23:50:36 +0500

    checking user

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Typography, Button } from "@material-ui/core";
+import * as CheckUser from "../../Constants/CheckUser";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -18,18 +19,27 @@ class Dashboard extends Component {
     // firebase.database().ref('')
   }
 
+  componentDidMount() {
+    CheckUser.isUser();
+  }
+  componentDidUpdate() {
+    CheckUser.isUser();
+  }
+
   render() {
     // console.group("DASHBOARD")
     // console.log(this.props, "****props");
     // console.groupEnd()
 
+    let user = JSON.parse(localStorage.getItem('user'))
+    console.log('dashboard.js', user);
+
     return (
       <div >
-        {/* <h1>Dashboard.js</h1> */}
-        <div style={{ textAlign: "center" }}>
+        {user && <div style={{ textAlign: "center" }}>
           <Typography variant="h6" style={{ lineHeight: "100px" }}>You haven’t done any meeting yet!</Typography>
           <Button variant="raised" color="primary" onClick={this.handleClick}>Set a meeting!</Button>
-        </div>
+        </div>}
       </div>
     );
   }
