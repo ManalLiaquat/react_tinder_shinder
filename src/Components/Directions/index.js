5985a4863813086d8f39c1907464d8b2a7a12e5a
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-27 23:36:14 +0500

    This component will show directions on google maps

---
+++ src/Components/Directions/index.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-undef */
+/* global google */
+import React, { Component } from "react";
+import { Button, Typography } from "@material-ui/core";
+import { withGoogleMap, GoogleMap, Marker, DirectionsRenderer, withScriptjs } from "react-google-maps"
+
+class Directions extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      err: ""
+    }
+    this.getDirections = this.getDirections.bind(this)
+  }
+
+  getDirections() {
+    const { userLocation, placeLocation } = this.props
+    const DirectionsService = new google.maps.DirectionsService();
+
+    DirectionsService.route({
+      origin: new google.maps.LatLng(userLocation.latitude, userLocation.longitude),
+      destination: new google.maps.LatLng(placeLocation.latitude, placeLocation.longitude),
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (result, status) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        this.setState({
+          directions: result,
+        });
+      } else {
+        this.setState({
+          err: "Sorry! Can't calculate directions!"
+        })
+      }
+    });
+  }
+
+  render() {
+    const { directions, err } = this.state
+    const { userLocation, placeLocation } = this.props
+
+    return (
+      <div>
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBrrOsrvThKXpEt-1ZoAP9DhpwRs1B5l4E&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          directions={directions}
+          userLocation={userLocation}
+          placeLocation={placeLocation}
+        />
+        <Button variant="contained" color="primary" onClick={this.getDirections}>Get Directions</Button>
+        <Typography variant="caption" color="secondary" align="right">{err}</Typography>
+      </div>
+    )
+  }
+}
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={14}
+    center={{ lat: props.userLocation.latitude, lng: props.userLocation.longitude }}
+  >
+
+    <Marker position={{ lat: props.userLocation.latitude, lng: props.userLocation.longitude }} />
+    <Marker position={{ lat: props.placeLocation.latitude, lng: props.placeLocation.longitude }} />
+
+    {props.directions && <DirectionsRenderer directions={props.directions} />}
+
+  </GoogleMap>
+))
+
+export default Directions
