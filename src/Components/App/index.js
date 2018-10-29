06e8dd4b0e64f34e70a4a782412cf91be4921898
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-29 22:48:17 +0500

    getting notification and showing popup

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import AppBar from '../MaterialUI/AppBar';
-
+import firebase from "../../Config/firebase";
+import Toast from "../../Constants/Toast";
 // let user = JSON.parse(localStorage.getItem('user'))
+firebase.messaging().onMessage(payload => {
+  // console.log(payload);
+
+  for (const key in payload.data) {
+    console.log("object", JSON.parse(payload.data[key]));
+  }
+
+  Toast({
+    type: "info",
+    title: payload.notification.title,
+    timer: 5000,
+    allowOutsideClick: false
+  })
+})
 
 const App = (props) => {
   return (
