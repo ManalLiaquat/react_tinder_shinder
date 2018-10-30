328b1a033301d7ab19a9e0ddf66a85f27ccc2b9b
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-30 21:42:36 +0500

    changing status of meeting request by button

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { Typography, Button, Paper, AppBar, Tabs, Tab, Avatar, List, ListItemText, IconButton, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from "@material-ui/core";
+import { Typography, Button, Tooltip, AppBar, Tabs, Tab, Avatar, List, ListItemText, IconButton, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from "@material-ui/core";
 import { withStyles } from '@material-ui/core/styles';
 import * as CheckUser from "../../Constants/CheckUser";
 import firebase from "../../Config/firebase";
-import Add from "@material-ui/icons/Add";
+import Toast from "../../Constants/Toast";
 import moment from "moment";
+import Add from "@material-ui/icons/Add";
 import CancelIcon from "@material-ui/icons/Cancel";
 import DoneIcon from "@material-ui/icons/DoneOutline";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -15,8 +16,17 @@ const styles = theme => ({
     position: 'absolute',
     bottom: theme.spacing.unit * 2,
     right: theme.spacing.unit * 2,
+  },
+  bigAvatar: {
+    width: 60,
+    height: 60,
+  },
+  listDisplayName: {
+    marginLeft: 10,
+    lineHeight: "60px"
   }
 });
+
 var user = JSON.parse(localStorage.getItem('user'))
 
 
@@ -28,32 +38,28 @@ class Dashboard extends Component {
       requests: [],
       tab: 0
     };
-    this.handleClick = this.handleClick.bind(this)
+    this.setMeeting = this.setMeeting.bind(this)
     this.tabChange = this.tabChange.bind(this)
   }
 
-  handleClick() {
+  setMeeting() {
     this.props.history.push('/meetings')
   }
 
   getUserMeetings() {
     let { meetings } = this.state
-    firebase.database().ref(`/meetings/${CheckUser.User.uid}`).on('child_added', data => {
+    firebase.database().ref(`/meetings/${user.uid}`).on('child_added', data => {
       let meetData = data.val()
       meetings.push(meetData)
-      console.log(meetData);
-
       this.setState({ meetings })
     })
   }
 
   getUserRequests() {
     let { requests } = this.state
-    firebase.database().ref(`/requests/${CheckUser.User.uid}`).on('child_added', data => {
+    firebase.database().ref(`/requests/${user.uid}`).on('child_added', data => {
       let reqData = data.val()
       requests.push(reqData)
-      console.log(reqData);
-
       this.setState({ requests })
     })
   }
@@ -62,7 +68,30 @@ class Dashboard extends Component {
     this.setState({ tab })
   }
 
+  handleStatus(meetingObj, status) {
+    meetingObj.status = status
+    firebase.database().ref(`/requests/${user.uid}/${meetingObj.friendProfileObj.uid}/`).set(meetingObj)
+      .then(() => {
+        let newObject = {
+          dateAndTime: meetingObj.dateAndTime,
+          friendProfileObj: meetingObj.myProfileObj,
+          myProfileObj: meetingObj.friendProfileObj,
+          placeInfo: meetingObj.placeInfo,
+          status: meetingObj.status
+        }
+        firebase.database().ref(`/meetings/${meetingObj.friendProfileObj.uid}/${user.uid}/`).set(newObject)
+          .then(() => {
+            Toast({
+              type: "success",
+              title: `Request has been ${status.toLowerCase()}`
+            })
+          })
+      })
+  }
+
+
   renderLists(array, showBtn) {
+    const { classes } = this.props
     return (
       <List style={{ width: "90%", margin: "0px auto" }}>
         {
@@ -70,19 +99,23 @@ class Dashboard extends Component {
             return (
               <ExpansionPanel key={item.friendProfileObj.uid}>
                 <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                  <Avatar alt={item.friendProfileObj.nickName} src={item.friendProfileObj.images[0]} />
-                  <Typography variant="headline" style={{ marginLeft: "10px" }}>{item.friendProfileObj.displayName}</Typography>
+                  <Avatar alt={item.friendProfileObj.nickName} src={item.friendProfileObj.images[0]} className={classes.bigAvatar} />
+                  <Typography variant="headline" className={classes.listDisplayName} >{item.friendProfileObj.displayName}</Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                   <ListItemText primary={`NickName: ${item.friendProfileObj.nickName}`} secondary={`Meeting Time: ${moment(item.dateAndTime).format('LLLL')}`} />
                   <ListItemText primary={`Status: ${item.status}`} secondary={`Location: ${item.placeInfo.name}, ${item.placeInfo.location.address}`} />
                   {showBtn && <div>
-                    <IconButton onClick={() => { console.log('Hello Icon') }} color="secondary">
-                      <CancelIcon />
-                    </IconButton>
-                    <IconButton onClick={() => { console.log('Hello Icon') }} style={{ color: green[800] }}>
-                      <DoneIcon />
-                    </IconButton>
+                    <Tooltip title="Cancel" disableFocusListener placement="top">
+                      <IconButton disabled={item.status !== 'PENDING' ? true : false} onClick={() => { this.handleStatus(item, "CANCELLED") }} color="secondary">
+                        <CancelIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Accept" disableFocusListener placement="top">
+                      <IconButton disabled={item.status !== 'PENDING' ? true : false} onClick={() => { this.handleStatus(item, "ACCEPTED") }} style={{ color: item.status !== 'PENDING' ? 'grey' : green[800] }}>
+                        <DoneIcon />
+                      </IconButton>
+                    </Tooltip>
                   </div>}
                 </ExpansionPanelDetails>
               </ExpansionPanel>
@@ -106,29 +139,27 @@ class Dashboard extends Component {
     const { classes } = this.props;
     const { meetings, tab, requests } = this.state;
     return (
-      <div >
-        <AppBar position='static' color='inherit'>
-          <Tabs value={tab} onChange={this.tabChange} indicatorColor="primary" textColor="primary" centered>
-            <Tab label="Meetings" />
-            <Tab label="Requests" />
-          </Tabs>
-        </AppBar>
-
+      <div>
         {user && <div style={{ textAlign: "center" }}>
+          <AppBar position='static' color='inherit'>
+            <Tabs value={tab} onChange={this.tabChange} indicatorColor="primary" textColor="primary" centered>
+              <Tab label="Meetings" />
+              <Tab label="Requests" />
+            </Tabs>
+          </AppBar>
           {tab === 0 && <div>
             {!meetings.length
-              ? <Typography variant="h6" style={{ lineHeight: "100px" }}>You haven’t done any meeting yet!</Typography>
+              ? <Typography variant="h6" style={{ lineHeight: "100px" }}>You haven’t set any meeting yet!</Typography>
               : this.renderLists(meetings, false)
             }
           </div>}
           {tab === 1 && <div>
             {!requests.length
-              ? <Typography variant="h6" style={{ lineHeight: "100px" }}>You haven’t done any meeting yet!</Typography>
+              ? <Typography variant="h6" style={{ lineHeight: "100px" }}>You haven’t any meeting request yet!</Typography>
               : this.renderLists(requests, true)
             }
           </div>}
-
-          <Button variant="extendedFab" aria-label="Add" color="primary" size="large" className={classes.fab} onClick={this.handleClick}>
+          <Button variant="extendedFab" aria-label="Add" color="primary" size="large" className={classes.fab} onClick={this.setMeeting}>
             <Add />
             Set a meeting!
           </Button>
