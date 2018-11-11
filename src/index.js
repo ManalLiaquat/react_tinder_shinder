14fcae5911076b5fc311c0eb014cc1cd9209fa0f
Manal Liaquat <m.manalliaquat@gmail.com>
2018-11-11 11:21:59 +0500

    using redux-persist to connect firebase authState with localStorage

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ import CustomRoutes from "./Routes";
 import * as serviceWorker from "./serviceWorker";
 import "./Config/firebase";
 import { Provider } from "react-redux";
-import store from "./Config/Redux/store";
+import { store, persistor } from "./Config/Redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
-ReactDOM.render(<Provider store={store}><Router><CustomRoutes /></Router></Provider>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <CustomRoutes />
+      </Router>
+    </PersistGate>
+  </Provider>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
