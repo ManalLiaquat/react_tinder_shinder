d12e044d280fa3de00ed546e978daacf22736e5a
Manal Liaquat <m.manalliaquat@gmail.com>
2018-11-12 23:44:02 +0500

    changing name profile to createProfile

---
+++ src/Components/CreateProfile/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import Stepper from "../MaterialUI/Stepper";
+
+const CreateProfile = (props) => {
+  return (
+    <div>
+      <Typography variant="body1" align="center" >Fill all fields, upload all (3) images, select atleast one from two diffrent questions and turn on location (accept location access).</Typography>
+      <Stepper {...props} />
+    </div>
+  );
+}
+
+export default CreateProfile;
