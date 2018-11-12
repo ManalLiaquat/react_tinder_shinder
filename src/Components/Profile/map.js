6fc39200cc7e97eb41110c47912d64ae61717157
Manal Liaquat <m.manalliaquat@gmail.com>
2018-11-12 23:43:17 +0500

    using react-google-maps library for map

---
+++ src/Components/Profile/map.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={18}
+      center={{ lat: props.coords.latitude, lng: props.coords.longitude }}
+    >
+      {props.isMarkerShown &&
+        props.coords && (
+          <Marker
+            draggable={false}
+            position={{
+              lat: props.coords.latitude,
+              lng: props.coords.longitude
+            }}
+          />
+        )}
+    </GoogleMap>
+  ))
+);
+
+export default MyMapComponent;
