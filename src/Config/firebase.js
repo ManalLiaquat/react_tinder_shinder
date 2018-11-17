7d96f645ce539d7ca6df6eb0f111297968c75ce8
Manal Liaquat <m.manalliaquat@gmail.com>
2018-11-17 15:12:43 +0500

    removing api key

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -1,11 +1,11 @@
 import * as firebase from "firebase";
 var config = {
-  apiKey: "AIzaSyBMtjjZLz2ekZtaK4QyaJUz1ssFV_PNH0g",
-  authDomain: "tinder-shinder-2.firebaseapp.com",
-  databaseURL: "https://tinder-shinder-2.firebaseio.com",
-  projectId: "tinder-shinder-2",
-  storageBucket: "tinder-shinder-2.appspot.com",
-  messagingSenderId: "1051289629097"
+  apiKey: "#######################",
+  authDomain: "#######################",
+  databaseURL: "#######################",
+  projectId: "#######################",
+  storageBucket: "#######################",
+  messagingSenderId: "#######################"
 };
 firebase.initializeApp(config);
 export default firebase;
