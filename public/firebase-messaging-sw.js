bf0131bb470b70ca41a46df66b0814edf2de511c
Manal Liaquat <m.manalliaquat@gmail.com>
2018-11-17 15:12:57 +0500

    senderid removing

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/5.5.5/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.5.5/firebase-messaging.js');
 
 firebase.initializeApp({
-  messagingSenderId: '1051289629097'
+  messagingSenderId: '#######################'
 });
 
 const messaging = firebase.messaging();
